f412ce6f55bb5fcf8d18008abcfde21a49990ee7
tcbrakefield2 <253540456+tcbrakefield2@users.noreply.github.com>
2026-03-11 11:26:49 -0500

    Update login redirection paths to use root

--- scripts/login.js
+++ scripts/login.js
@@ -7,13 +7,12 @@ if(orderLink){
     const loggedIn = false; 
     
     if(loggedIn) {
-      window.location.href = "pages/order-page.html";
+      window.location.href = "/pages/order-page.html";
     }
     else if (!loggedIn) {
-      window.location.href = "pages/login-page.html";
+      window.location.href = "/pages/login-page.html";
     }
-    
-
   });
 }
+
 
