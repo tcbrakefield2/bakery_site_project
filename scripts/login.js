9d080cf08d7615aa6637d85f9ebde776c85d482a
tcbrakefield2 <253540456+tcbrakefield2@users.noreply.github.com>
2026-03-11 11:11:50 -0500

    Fix navigation links for order and login pages

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,15 @@
   const orderLink = document.getElementById('order-link');
 
   orderLink.addEventListener('click', function(event){
-    event.preventDefault(); // prevent default navigation
-    //check login is true or not. if login, straigt to order page
+    event.preventDefault(); 
+    
     const loggedIn = false; 
     if(loggedIn) {
-      window.location.href = '../pages/order-page.html'; // go to login page
+      window.location.href = '/pages/order-page.html';
     }
     else if (!loggedIn) {
-      window.location.href = '../pages/login-page.html'; // go to login page
+      window.location.href = '/pages/login-page.html';
     }
     
+
   });
