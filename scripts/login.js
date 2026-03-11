47ed8ad507ab6862955988006c3738900bc0a0ad
tcbrakefield2 <253540456+tcbrakefield2@users.noreply.github.com>
2026-03-11 11:24:38 -0500

    Add null check for orderLink element
    Add null check for orderLink before adding event listener.

--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,19 @@
   const orderLink = document.getElementById('order-link');
 
+if(orderLink){
   orderLink.addEventListener('click', function(event){
     event.preventDefault(); 
     
     const loggedIn = false; 
+    
     if(loggedIn) {
-      window.location.href = 'pages/order-page.html';
+      window.location.href = "pages/order-page.html";
     }
     else if (!loggedIn) {
-      window.location.href = 'pages/login-page.html';
+      window.location.href = "pages/login-page.html";
     }
     
 
   });
+}
 
