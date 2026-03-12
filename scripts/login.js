c9db2fe3dbd52f73c931367ddc2e254e4f9a4cf2
tcbrakefield2 <253540456+tcbrakefield2@users.noreply.github.com>
2026-03-11 19:34:16 -0500

    Implement logout button and session-based login check
    Added a logout button for users and updated login check.

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,41 @@
   const orderLink = document.getElementById('order-link');
 
+
+
+//if login = false, then create a button. 
+document.addEventListener('DOMContentLoaded', ()=>{
+
+
+
+  console.log('dom loaded? ')
+  //check login status.. 
+  //if getItem ... true... 
+
+  let logoutBtn = document.createElement("button"); 
+  
+  logoutBtn.textContent = 'logout'; 
+  // logoutBtn.classList('btn btn-secondary'); 
+  // add a click event...remove session = false. 
+  
+  orderLink.parentNode.insertBefore(logoutBtn, orderLink.nextSibling);
+
+});  
+
+
 if(orderLink){
   orderLink.addEventListener('click', function(event){
     event.preventDefault(); 
     
-    const loggedIn = false; 
-    
+    // const loggedIn = false; 
+    const loggedIn = sessionStorage.getItem('loggedIn');
+  
+
     if(loggedIn) {
-      window.location.href = "order-page.html";
+      window.location.href = "../pages/order-page.html";
     }
       else if (!loggedIn) {
-      window.location.href = "login-page.html";
+      window.location.href = "../pages/login-page.html";
       }
   });
 }
-
-
-
-
-
-
 
